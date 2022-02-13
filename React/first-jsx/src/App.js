7d29960c4815af0ea9a8bb11ceadae15700d381f
bo98117 <bosin98117@gmail.com>
2022-02-13 08:10:51 -0800

    First JSX commit

---
+++ React/first-jsx/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <p>* Learn React</p>
+      <p>* Climb Mt. Everest</p>
+      <p>* Run a marathon</p>
+      <p>* Feed the dogs</p>
+    </>
+  );
+}
+
+export default App;
