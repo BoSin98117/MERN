5686ddfbf8957439c21b08c6ecd1a0b981c20789
bo98117 <bosin98117@gmail.com>
2022-02-13 08:16:51 -0800

    Prop it up src

---
+++ React/prop-it-up/src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import PersonCard from './components/PersonCard';
+// import UserForm from './components/UserForm';
+// import HookForm from './components/HookForm';
+// import HandlingValidations from './components/HandlingValidations';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonCard 
+      firstName={"Bo"} 
+      lastName={"Sin"} 
+      age={ 43 } 
+      hairColor={"bald"} />
+
+      <PersonCard 
+      firstName={"Jayden"} 
+      lastName={"Sin"} 
+      age={ 10 } 
+      hairColor={"black"} />
+
+<PersonCard 
+      firstName={"John"} 
+      lastName={"Doe"} 
+      age={ 25 } 
+      hairColor={"gray"} />
+
+<PersonCard 
+      firstName={"Jane"} 
+      lastName={"Doe"} 
+      age={ 35 } 
+      hairColor={"blonde"} />
+
+      {/* <UserForm /> */}
+
+      {/* <HookForm /> */}
+
+      {/* <HandlingValidations /> */}
+      
+    </div>
+    
+  );
+}
+
+export default App;
